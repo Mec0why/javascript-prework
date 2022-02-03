e8b08dee6f5dfc0b925933acdab46585a0019579
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-03 21:24:47 +0100

    Add JS playerMove Variable

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,9 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+let computerMove = `kamień`;
+	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let computerMove = 'papier';
+	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
---
+++ js/script.js
@@ -0,0 +1,2 @@
+let computerMove = 'papier';
+	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
