cf9562af1ca54d2da9ad59725b60f80baacdc62f
Mikolaj Renke <mikolaj.renke@gmail.com>
2022-02-05 19:40:36 +0100

    Add JS Player + Computer Moves

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
-let computerMove = `kamień`;
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);​
 
-let playerMove = 'papier';
-	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+	computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+	computerMove = 'papier';
+}
+else if(randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+​let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+	playerMove = 'kamień';
+} 
+else if(playerInput == 2){
+	playerMove = 'papier';
+}
+else if(playerInput == 3){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if(playerMove > computerMove) {
+	printMessage('Gratulacje! Wygrałeś!'); 
+}
+else { printMessage('Spróbuj Ponownie');
+}
